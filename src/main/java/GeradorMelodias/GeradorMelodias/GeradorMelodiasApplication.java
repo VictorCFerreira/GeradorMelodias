e89a4f132ebc8c3e44c3d3686d41574c74720e02
Victor <47982205+VictorCFerreira@users.noreply.github.com>
2024-10-15 19:06:04 -0300

    Initial commit

---
+++ src/main/java/GeradorMelodias/GeradorMelodias/GeradorMelodiasApplication.java
@@ -0,0 +1,13 @@
+package GeradorMelodias.GeradorMelodias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeradorMelodiasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeradorMelodiasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/GeradorMelodias/GeradorMelodias/GeradorMelodiasApplicationTests.java
@@ -0,0 +1,13 @@
+package GeradorMelodias.GeradorMelodias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeradorMelodiasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
