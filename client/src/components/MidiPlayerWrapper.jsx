df239fd052b95d5cecbd9836defa1b72c036c931
DesenvolvimentoVisionCar <47982205+VictorCFerreira@users.noreply.github.com>
2025-01-28 15:17:20 -0300

    delete classes não mais utilizadas

--- client/src/components/MidiPlayerWrapper.jsx
+++
@@ -1,9 +0,0 @@
-import { forwardRef } from 'react';
-import  React from 'react';
-import MidiPlayer from 'react-midi-player';
-
-const MidiPlayerWrapper = forwardRef((props, ref) => {
-  return <MidiPlayer {...props} forwardedRef={ref} />;
-});
-
-export default MidiPlayerWrapper;
