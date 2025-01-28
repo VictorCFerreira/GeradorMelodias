c7c2624b9234a82676792234aca644607f92a6e7
Victor <47982205+VictorCFerreira@users.noreply.github.com>
2025-01-27 22:23:13 -0300

    testando midi.js

--- client/src/components/MidiPlayer.jsx
+++ client/src/components/MidiPlayer.jsx
@@ -1,79 +1,197 @@
 import React, { useState, useEffect, useRef } from "react";
-import * as Tone from "tone";
-import { Midi } from "@tonejs/midi";
 import { FaPause, FaPlay, FaDownload } from "react-icons/fa";
+import axios from "axios";
 
 const MidiPlayer = ({ midiBase64 }) => {
   const [isPlaying, setIsPlaying] = useState(false);
   const [progress, setProgress] = useState(0);
-  const midiDurationRef = useRef(15);
+  const [midiDuration, setMidiDuration] = useState(0);
+  const [player, setPlayer] = useState();
   const intervalRef = useRef(null);
-  const synth = useRef(null);
 
-  const base64ToArrayBuffer = (base64) => {
-    const binaryString = atob(base64);
-    const len = binaryString.length;
-    const bytes = new Uint8Array(len);
-    for (let i = 0; i < len; i++) {
-      bytes[i] = binaryString.charCodeAt(i);
-    }
-    return bytes.buffer;
-  };
+  useEffect(() => {
+    // Definir Base64Binary no escopo global para garantir que MIDI.js possa acessá-lo
+    window.Base64Binary = {
+      decodeArrayBuffer: function (base64) {
+        var binary_string = atob(base64);
+        var len = binary_string.length;
+        var bytes = new Uint8Array(len);
+        for (var i = 0; i < len; i++) {
+          bytes[i] = binary_string.charCodeAt(i);
+        }
+        return bytes.buffer;
+      },
+    };
 
-  const playMidi = async () => {
-    try {
+    // Carregar o MIDI.js e configurar o player
+    const script = document.createElement("script");
+    script.src = "/js/MIDI.js"; // Certifique-se de que midi.js está no caminho correto
+    script.async = true;
+    script.onload = () => {
+      // Verificar se o MIDI.js foi carregado corretamente
+      console.log("midi: " + typeof MIDI);
+
+      if (typeof MIDI !== "undefined") {
+        console.log("MIDI.js carregado.");
+
+        // Usando Axios para carregar o SoundFont
+        axios
+          .get("/soundfonts/acoustic_grand_piano-mp3-ogg.js")
+          .then((response) => {
+            console.log("Resposta do SoundFont:", response);
+
+            // Em vez de usar eval, tente usar uma abordagem de importação ou execução direta:
+            // Exemplo de executar diretamente o código JavaScript retornado
+            const script = document.createElement("script");
+            script.innerHTML = response.data;
+            document.body.appendChild(script);
+
+            MIDI.loadPlugin({
+              soundfontUrl: "/soundfonts/",
+              instrument: "acoustic_grand_piano-mp3",
+              onsuccess: () => {
+                setPlayer(MIDI.Player);
+                console.log("Plugin de som carregado com sucesso.");
+              },
+              onerror: (err) => {
+                console.error("Erro ao carregar o plugin de som:", err);
+              },
+            });
+          })
+          .catch((err) => {
+            console.error("Erro ao carregar o SoundFont:", err);
+          });
+
+      } else {
+        console.error("MIDI.js não foi carregado corretamente.");
+      }
+    };
+
+    script.onerror = () => {
+      console.error("Erro ao carregar o script MIDI.js.");
+    };
+    document.body.appendChild(script);
+
+    return () => {
       if (intervalRef.current) {
         clearInterval(intervalRef.current);
       }
+    };
+  }, []);
 
-      Tone.Transport.stop();
-      Tone.Transport.cancel();
-      Tone.Transport.seconds = 0;
 
-      setProgress(0);
+  const playMidi = () => {
+    try {
+      // Converte o base64 em ArrayBuffer
+      const midiArrayBuffer = Base64Binary.decodeArrayBuffer(midiBase64);
+  
+      // Verifica se MIDI.js está disponível
+      // if (typeof MIDI === 'undefined') {
+      //   console.error("MIDI.js não está disponível.");
+      //   return;
+      // }
+  
+      // Verifica se o MIDI.Player está disponível
+      // if (!MIDI.Player) {
+      //   console.error("MIDI.Player não está disponível.");
+      //   return;
+      // }
+  
+      //Verifica se o MIDI.Player foi carregado corretamente
+      
 
-      // Converter o MIDI base64 para ArrayBuffer
-      const midiArrayBuffer = base64ToArrayBuffer(midiBase64);
-      const midi = new Midi(midiArrayBuffer);
+      MIDI.loadPlugin({
+        soundfontUrl: "/soundfonts/",
+        instrument: "acoustic_grand_piano-mp3",
+        onsuccess: () => {
+          if (!MIDI.Player.loaded) {
+            console.error("O plugin MIDI não foi carregado corretamente.");
+            return;
+          }
 
-      // Criar o sintetizador
-      synth.current = new Tone.PolySynth().toDestination();
+          MIDI.Player.loadFile(midiUrl, () => {
+            // Verificar se o arquivo MIDI foi carregado corretamente
+            if (MIDI.Player.endTime === 0) {
+              console.error("O arquivo MIDI não foi carregado corretamente.");
+              return;
+            }
 
-      // Iniciar o sintetizador
-      midi.tracks.forEach((track) => {
-        track.notes.forEach((note) => {
-          synth.current.triggerAttackRelease(
-            note.name,
-            note.duration,
-            note.time,
-            note.velocity
-          );
-        });
+            if (!MIDI.Player.loaded) {
+              console.error("O plugin MIDI não foi carregado corretamente.");
+              return;
+            }
+      
+            // Atribuir a duração do MIDI
+            setMidiDuration(MIDI.Player.endTime);
+      
+            // Iniciar o playback do MIDI
+            MIDI.Player.start();
+            setIsPlaying(true);
+      
+            // Atualizar o progresso da música
+            intervalRef.current = setInterval(() => {
+              setProgress((MIDI.Player.currentTime / MIDI.Player.endTime) * 100);
+            }, 100);
+      
+            // Parar o intervalo quando a música terminar
+            const checkPlaybackEnd = setInterval(() => {
+              if (MIDI.Player.currentTime >= MIDI.Player.endTime) {
+                clearInterval(intervalRef.current);
+                clearInterval(checkPlaybackEnd);
+                setIsPlaying(false);
+              }
+            }, 100);
+          });
+          console.log("Plugin de som carregado com sucesso.");
+        },
+        onerror: (err) => {
+          console.error("Erro ao carregar o plugin de som:", err);
+        },
       });
-
-      await Tone.start();
-      Tone.Transport.start();
-      setIsPlaying(true);
-
-      // Atualização do andamento da melodia
-      intervalRef.current = setInterval(() => {
-        const elapsedTime = Tone.Transport.seconds;
-        const progressValue = (elapsedTime / midiDurationRef.current) * 100;
-        setProgress(progressValue);
-
-        if (progressValue >= 100) {
-          clearInterval(intervalRef.current);
-          Tone.Transport.stop();
-          setIsPlaying(false);
+    
+  
+      // Criar um Blob do ArrayBuffer e gerar um URL temporário
+      const midiBlob = new Blob([midiArrayBuffer], { type: 'audio/midi' });
+      const midiUrl = URL.createObjectURL(midiBlob);
+  
+      // Carregar o arquivo MIDI a partir da URL
+      MIDI.Player.loadFile(midiUrl, () => {
+        // Verificar se o arquivo MIDI foi carregado corretamente
+        if (MIDI.Player.endTime === 0) {
+          console.error("O arquivo MIDI não foi carregado corretamente.");
+          return;
         }
-      }, 100);
+  
+        // Atribuir a duração do MIDI
+        setMidiDuration(MIDI.Player.endTime);
+  
+        // Iniciar o playback do MIDI
+        MIDI.Player.start();
+        setIsPlaying(true);
+  
+        // Atualizar o progresso da música
+        intervalRef.current = setInterval(() => {
+          setProgress((MIDI.Player.currentTime / MIDI.Player.endTime) * 100);
+        }, 100);
+  
+        // Parar o intervalo quando a música terminar
+        const checkPlaybackEnd = setInterval(() => {
+          if (MIDI.Player.currentTime >= MIDI.Player.endTime) {
+            clearInterval(intervalRef.current);
+            clearInterval(checkPlaybackEnd);
+            setIsPlaying(false);
+          }
+        }, 100);
+      });
     } catch (error) {
-      console.error("Erro ao processar o MIDI:", error.message);
+      console.error("Erro ao tocar o MIDI:", error.message);
     }
   };
+  
+
 
   const pauseMidi = () => {
-    Tone.Transport.pause();
+    MIDI.Player.pause();
     setIsPlaying(false);
     if (intervalRef.current) {
       clearInterval(intervalRef.current);
@@ -86,16 +204,6 @@ const MidiPlayer = ({ midiBase64 }) => {
     link.download = "melody.mid";
     link.click();
   };
-
-  useEffect(() => {
-    return () => {
-      if (intervalRef.current) {
-        clearInterval(intervalRef.current);
-      }
-      Tone.Transport.stop();
-      Tone.Transport.cancel();
-    };
-  }, []);
 
   return (
     <div className="p-4 w-full max-w-sm mx-auto bg-blue-300 rounded-lg shadow-md">
